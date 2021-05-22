c6e4dcd546ddc47a313aa44c1ae845112bb60f2a
Diogo Trindade <diogotr7@gmail.com>
2021-05-22 23:27:42 +0100

    fixed pre requisites

--- src/Artemis.Plugins.Wrappers.Logitech/Prerequisites/LogitechWrapperPrerequisite.cs
+++ src/Artemis.Plugins.Wrappers.Logitech/Prerequisites/LogitechWrapperPrerequisite.cs
@@ -19,32 +19,24 @@ namespace Artemis.Plugins.Wrappers.Logitech.Prerequisites
 
         public override bool IsMet()
         {
-            //this pre requisite *should* set both 32 and 64 bit keys, so we'll only check one of them :)
-            RegistryKey key64 = Registry.LocalMachine.OpenSubKey(REGISTRY_PATH_64);
-            if (key64 == null)
-            {
-                return false;
-            }
+            using RegistryKey key64 = Registry.LocalMachine.OpenSubKey(REGISTRY_PATH_64);
+            using RegistryKey key32 = Registry.LocalMachine.OpenSubKey(REGISTRY_PATH_32);
 
-            string dllPath = key64.GetValue(null)?.ToString();
+            bool is64BitKeyPresent = key64?.GetValue(null)?.ToString() == _wrapperPath64;
+            bool is32BitKeyPresent = key32?.GetValue(null)?.ToString() == _wrapperPath32;
 
-            if (dllPath == null)
-            {
-                return false;
-            }
-
-            return dllPath.Contains(DLL_NAME);
+            return is64BitKeyPresent && is32BitKeyPresent;
         }
 
         public LogitechWrapperPrerequisite(Plugin plugin)
         {
-            string wrapperPath64 = Path.Combine(plugin.Directory.FullName, "x64", DLL_NAME);
-            string wrapperPath32 = Path.Combine(plugin.Directory.FullName, "x86", DLL_NAME);
+            _wrapperPath64 = Path.Combine(plugin.Directory.FullName, "x64", DLL_NAME);
+            _wrapperPath32 = Path.Combine(plugin.Directory.FullName, "x86", DLL_NAME);
 
             InstallActions = new List<PluginPrerequisiteAction>
             {
-                new WriteLogitechRegistryPathAction("Patch 64 bit registry", REGISTRY_PATH_64, wrapperPath64),
-                new WriteLogitechRegistryPathAction("Patch 32 bit registry", REGISTRY_PATH_32, wrapperPath32)
+                new WriteLogitechRegistryPathAction("Patch 64 bit registry", REGISTRY_PATH_64, _wrapperPath64),
+                new WriteLogitechRegistryPathAction("Patch 32 bit registry", REGISTRY_PATH_32, _wrapperPath32)
             };
 
             UninstallActions = new List<PluginPrerequisiteAction>
@@ -56,5 +48,7 @@ namespace Artemis.Plugins.Wrappers.Logitech.Prerequisites
         private const string DLL_NAME = "Artemis.Wrapper.Logitech.dll";
         private const string REGISTRY_PATH_64 = "SOFTWARE\\Classes\\CLSID\\{a6519e67-7632-4375-afdf-caa889744403}\\ServerBinary";
         private const string REGISTRY_PATH_32 = "SOFTWARE\\Classes\\WOW6432Node\\CLSID\\{a6519e67-7632-4375-afdf-caa889744403}\\ServerBinary";
+        private readonly string _wrapperPath64;
+        private readonly string _wrapperPath32;
     }
 }
